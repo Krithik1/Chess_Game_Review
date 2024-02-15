c91b9daefc00c0f891aadec04f52b2b44ddd58d0
Krithik <krithiksehgal@gmail.com>
2024-02-15 02:05:26 -0800

    Fix Linter Issues and size of chess board

--- get_games.py
+++ get_games.py
@@ -1,16 +1,27 @@
+"""Get the games for a given user, year, and month."""
+
 import re
 
 from chessdotcom import Client, get_player_games_by_month_pgn
 
-Client.request_config["headers"]["User-Agent"] = (
-    "My Python Application. " "Contact me at email@example.com"
-)
+Client.request_config["headers"][
+    "User-Agent"
+] = "My Python Application. Contact me at email@example.com"
 
 
 def get_games(username, year, month):
-    data = get_player_games_by_month_pgn(
-        username, year=int(year), month=int(month)
-    ).json["pgn"]["pgn"]
+    """Get the games for a given user, year, and month.
+
+    Args:
+        username (str): The username of the player.
+        year (str): The year of the games.
+        month (str): The month of the games.
+
+    Returns:
+        list: The list of games for the given user, year, and month.
+    """
+    data = get_player_games_by_month_pgn(username, year=int(year), month=int(month))
+    data = data.json["pgn"]["pgn"]  # pylint: disable=maybe-no-member
     data = data.split('[Event "Live Chess"]')[1:]
     games = []
     for game in data:
@@ -19,6 +30,14 @@ def get_games(username, year, month):
 
 
 def parse_game(game):
+    """Parse the game into a dictionary.
+
+    Args:
+        game (str): The game to parse.
+
+    Returns:
+        dict: The parsed game.
+    """
     d = {}
     # Index is a hash of the game and is unique and positive
     d["index"] = hash(game) % (10**8)
--- main.py
+++ main.py
@@ -1,74 +1,122 @@
+"""The main file for the application."""
+
 import chess
 import chess.engine
 import chess.pgn
 import chess.svg
 from flask import Flask, render_template
 
-from getGames import get_games
+from get_games import get_games
 
 app = Flask("testapp")
-svgs = []
-games = []
-curr_game = None
-move = 0
-variable = None
+
+
+class GameState:
+    """A class to store the state of the application."""
+
+    def __init__(self):
+        self.svgs = []
+        self.games = []
+        self.curr_game = None
+        self.move = 0
+        self.variable = None
+
+    def find_curr_game(self, idx):
+        """Find the current game with the given index.
+
+        Args:
+            idx (str): The index of the game.
+        """
+        for game in self.games:
+            if game["index"] == int(idx):
+                self.curr_game = game
+                break
+
+    def set_svgs(self):
+        """Set the SVGs for the game.
+
+        Args:
+            board (chess.Board): The board of the game.
+        """
+        board = chess.Board()
+        self.svgs = [chess.svg.board(board)]
+        for move in self.curr_game["moves"]:
+            board.push_san(move)
+            self.svgs.append(chess.svg.board(board))
+
+
+state = GameState()
 
 
 @app.route("/")
 def index():
+    """The home page of the application.
+
+    Returns:
+        str: The rendered HTML of the home page.
+    """
     return render_template("home.html")
 
 
 @app.route("/games/<username>/<year>/<month>")
-def games(username, year, month):
-    global games
-    games = get_games(username, year, month)
-    return render_template("games.html", games=games, username=username)
+def get_games_html(username, year, month):
+    """Get the games for a given user, year, and month.
+
+    Args:
+        username (str): The username of the player.
+        year (str): The year of the games.
+        month (str): The month of the games.
+
+    Returns:
+        str: The rendered HTML of the games page.
+    """
+    state.games = get_games(username, year, month)
+    return render_template("games.html", games=state.games, username=username)
 
 
-@app.route("/game/<index>")
-def game(index):
-    global svgs
-    global variable
-    global move
-    global games
-    global curr_game
-    for game in games:
-        if game["index"] == int(index):
-            curr_game = game
-            break
-    board = chess.Board()
-    svgs = [board._repr_svg_()]
-    for move in curr_game["moves"]:
-        board.push_san(move)
-        svgs.append(board._repr_svg_())
-    move = 0
-    variable = svgs[move]
-    return render_template("index.html", variable=variable)
+@app.route("/game/<idx>")
+def get_game(idx):
+    """Get the game with the given index.
+
+    Args:
+        idx (str): The index of the game.
+
+    Returns:
+        str: The rendered HTML of the game page.
+    """
+    state.find_curr_game(idx)
+    state.set_svgs()
+    state.move = 0
+    state.variable = state.svgs[state.move]
+    return render_template("index.html", variable=state.variable)
 
 
-@app.route("/game/<index>/next")
-def next(index):
-    global svgs
-    global move
-    global variable
-    move += 1
-    if move >= len(svgs):
-        move = len(svgs) - 1
-    variable = svgs[move]
-    return render_template("index.html", variable=variable)
+@app.route("/game/<idx>/next")
+def next_move(idx):
+    """Get the next move in the game.
+
+    Returns:
+        str: The rendered HTML of the game page.
+    """
+    print(idx)
+    state.move += 1
+    state.move = min(len(state.svgs) - 1, state.move)
+    state.variable = state.svgs[state.move]
+    return render_template("index.html", variable=state.variable)
 
 
-@app.route("/game/<index>/prev")
-def prev(index):
-    global svgs
-    global move
-    global variable
-    move -= 1
-    if move < 0:
-        move = 0
-    variable = svgs[move]
-    return render_template("index.html", variable=variable)
+@app.route("/game/<idx>/prev")
+def prev_move(idx):
+    """Get the previous move in the game.
+
+    Returns:
+        str: The rendered HTML of the game page.
+    """
+    print(idx)
+    state.move -= 1
+    state.move = max(0, state.move)
+    state.variable = state.svgs[state.move]
+    return render_template("index.html", variable=state.variable)
 
 
 if __name__ == "__main__":
